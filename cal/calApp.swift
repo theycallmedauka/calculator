4e90b79405299c22466330a29bbbc44f9a1f56b5
theycallmedauka <17niyazov@gmail.com>
2022-11-09 17:49:09 +0100

    Initial Commit

---
+++ cal/calApp.swift
@@ -0,0 +1,17 @@
+//
+//  calApp.swift
+//  cal
+//
+//  Created by Daulet Niyazov on 12.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct calApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
